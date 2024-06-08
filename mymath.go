97ade0d95f02176ba3cd5869fcae262a50df304f
gk <kotelnikovq@gmail.com>
2024-06-08 17:08:03 +0300

    add mymath.go

---
+++ mymath.go
@@ -0,0 +1,59 @@
+package mymath
+
+import "math"
+
+// Abs - функция для вычисления абсолютного значения числа
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
+
+// Sqrt - функция для вычисления квадратного корня из числа
+func Sqrt(x float64) float64 {
+	return math.Sqrt(x)
+}
+
+// Pow - функция для возведения числа в степень
+func Pow(x, y float64) float64 {
+	return math.Pow(x, y)
+}
+
+// Sin - функция для вычисления синуса
+func Sin(x float64) float64 {
+	return math.Sin(x)
+}
+
+// Cos - функция для вычисления косинуса
+func Cos(x float64) float64 {
+	return math.Cos(x)
+}
+
+// Tan - функция для вычисления тангенса
+func Tan(x float64) float64 {
+	return math.Tan(x)
+}
+
+// Log - функция для вычисления натурального логарифма
+func Log(x float64) float64 {
+	return math.Log(x)
+}
+
+// Exp - функция для вычисления экспоненты
+func Exp(x float64) float64 {
+	return math.Exp(x)
+}
+
+// Floor - функция для вычисления наибольшего целого числа, не большего x
+func Floor(x float64) float64 {
+	return math.Floor(x)
+}
+
+// Ceil - функция для вычисления наименьшего целого числа, не меньшего x
+func Ceil(x float64) float64 {
+	return math.Ceil(x)
+}
+func Yn(n int, x float64) float64 {
+	return math.Yn(n, x)
+}
+func Max(x float64, y float64) float64 {
+	return math.Max(x, y)
+}
